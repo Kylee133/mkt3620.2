cabfe57e2db1b2188fd972d040f25427de60c446
Kylee133 <kyleeb13@iastate.edu>
2026-09-10 13:38:56 -0500

    Fix nav styling - simplify nav structure to match CSS

--- nav.js
+++ nav.js
@@ -2,19 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const nav = document.createElement("nav");
 
   nav.innerHTML = `
-    <div class="nav-container">
-      <a href="index.html">
-        <img src="assets/logo.png" alt="Logo">
-      </a>
-
-      <ul class="nav-links">
-        <li><a href="index.html">About Me</a></li>
-        <li><a href="portfolio.html">Portfolio</a></li>
-        <li><a href="product.html">Products</a></li>
-      </ul>
-    </div>
+    <ul class="nav-links">
+      <li><a href="index.html">About Me</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="product.html">Products</a></li>
+    </ul>
   `;
 
-  nav.classList.add("site-nav");
   document.body.prepend(nav);
 });
